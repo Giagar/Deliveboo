e4b0249fbffd7156022a5d25eec3e49fc64ba438
GitFhubio <bagheeradonato@outlook.it>
2021-04-15 18:53:23 +0200

    rotte

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('categories','Api\ApiController@categories');
+Route::get('restaurants','Api\ApiController@restaurants');
+Route::get('categories/{name}','Api\ApiController@filteredRestaurants');
