d269970f34193503d02f254e2f370d4b1d7ebccb
GitFhubio <bagheeradonato@outlook.it>
2021-04-17 11:08:27 +0200

    prove di restituzione ordine

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Dish;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -13,10 +14,11 @@ class DashboardController extends Controller
     }
     public function statistics(){
         // qui usero javascript quindi passo un json
-        return view('user.statistics',compact('orders'));
+        return view('user.statistics',compact('dishes'));
     }
     public function orders(){
+        $dishes = Dish::where('user_id', Auth::id())->get();
         // questa pagina la gestiremo con blade
-        return view('user.orders',compact('orders'));
+        return view('user.orders',compact('dishes'));
     }
 }
