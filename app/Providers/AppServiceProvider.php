2fed5496eab7a6edbd92902153d3438be424efcd
Gianpaolo <pao.barga@gmail.com>
2021-04-19 01:12:05 +0200

    sistemato ricerca e aggiunto azzeramento campi diversi da quello active

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      \Braintree\Configuration::environment(env('BTREE_ENVIRONMENT','sandbox'));
-        \Braintree\Configuration::merchantId(env('BTREE_MERCHANT_ID','rn6zmvs8v98c9k5t'));
-        \Braintree\Configuration::publicKey(env('BTREE_PUBLIC_KEY','6xhdy4kpk8ktd3dt'));
-        \Braintree\Configuration::privateKey(env('BTREE_PRIVATE_KEY','b0ea0391e8114daf7472b68caba9697e'));
+    //   \Braintree\Configuration::environment(env('BTREE_ENVIRONMENT','sandbox'));
+    //     \Braintree\Configuration::merchantId(env('BTREE_MERCHANT_ID','rn6zmvs8v98c9k5t'));
+    //     \Braintree\Configuration::publicKey(env('BTREE_PUBLIC_KEY','6xhdy4kpk8ktd3dt'));
+    //     \Braintree\Configuration::privateKey(env('BTREE_PRIVATE_KEY','b0ea0391e8114daf7472b68caba9697e'));
     }
 }
 
--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -11,12 +11,13 @@
             <img :src="category.img" alt="">
             <h4>@{{category.name}}</h4>
         </div>
-        @if (Request::route()->getName() == 'landing')
+        {{-- @if (Request::route()->getName() == 'landing') --}}
               <form class="form-inline my-2 my-lg-0">
-                <input @keyup="filterByName()" class="form-control mr-sm-2" type="search" v-model="searchName" placeholder="Ricerca per nome" aria-label="Search">
-                <input @keyup="filterByAddress()" class="form-control mr-sm-2" type="search" v-model="searchAddress" placeholder="Ricerca per indirizzo" aria-label="Search">
-              </form>
-        @endif
+                <input @keyup="filterByName()" class="form-control mr-sm-2" type="search" v-model="searchName" placeholder="Ricerca per nome" aria-label="Search" id="searchByName" name="searchByName">
+                <input @keyup="filterByAddress()" class="form-control mr-sm-2" type="search" v-model="searchAddress" placeholder="Ricerca per indirizzo" aria-label="Search" id="searchByAdress" name="searchByAdress">
+                <input class="btn btn-danger" @click="showAll()" value="Reset">
+            </form>
+        {{-- @endif --}}
     </section>
 
     <h2 class="text-center" style="margin-top: 20px">Lasciati ispirare, ordina e ricevi comodamente a casa tua!</h2>
@@ -34,7 +35,7 @@
         {{-- /versione vecchia --}}
 
         {{-- versione nuova --}}
-        <div class="restaurants d-flex" :style="{'background-image':'url('+restaurant.img+')'}" v-for="(restaurant, index) in restaurants" v-if="(selected === 'All') && index < 9">
+        <div class="restaurants d-flex" :style="{'background-image':'url('+restaurant.img+')'}" v-for="(restaurant, index) in restaurants" v-if="(selected === 'All') && index < maxRestaurantShown">
             <a :href="'/restaurants/' + restaurant.restaurant_name">
                 <span>@{{restaurant.restaurant_name}}</span>
             </a>
