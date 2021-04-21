a4f49a55d8e25f8818830ba3b8de5b7d1fa8eeeb
diego-scano <diego.scano77@gmail.com>
2021-04-21 19:35:19 +0200

    grafica dashboard e orders

--- resources/views/user/orders.blade.php
+++ resources/views/user/orders.blade.php
@@ -1,3 +1,9 @@
+@extends('layouts.base')
+
+@section('title','Dashboard')
+
+@section('content')
+
 {{-- soluzione  a --}}
 
 {{-- @foreach ($dishes as $dish)
@@ -10,6 +16,53 @@
 
 {{-- soluzione b che è meglio --}}
 {{-- @dd($orders) --}}
+<div class="receipt-container">
 @foreach ($orders as $order)
-{{$order->customer_name}}
+
+<div class="receipt">
+
+    <div class="logo">
+        <img src="{{asset('images/logo.jpg')}}" alt="">
+    </div>
+
+    <div class="info">
+        <h3>Info Cliente</h3>
+        <p>
+            Nome          : {{$order->customer_name}}</br>
+            Cognome       : {{$order->customer_surname}}</br>
+            Indirizzo     : {{$order->customer_address}}</br>
+            Email         : {{$order->customer_email}}</br>
+            Ordinato il   : {{$order->created_at}}</br>
+        </p>
+    </div>
+
+        <div id="table">
+            <table class="table">
+                <thead class="thead-light">
+                    <tr>
+                        <th scope="col">Prodotto</th>
+                        <th scope="col">Quantità</th>
+                        <th scope="col">Subtotale</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($order->dishes as $dish)
+                    <tr>
+                        <td>{{$dish->name}}</td>
+                        <td>1</td>
+                        <td>€</td>
+                    </tr>
+                    @endforeach
+                    <tr class="total">
+                        <td></td>
+                        <td><b>Totale</b></td>
+                        <td><b>€</b></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+</div>
 @endforeach
+</div>
+
+@endsection
