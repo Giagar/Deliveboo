131575f43400697514bdd5c8d2f3f1c0ccc74423
diego-scano <diego.scano77@gmail.com>
2021-04-23 18:44:19 +0200

    pulsante back

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -55,7 +55,7 @@
                         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
                     </svg>
                     <h2 id="empty">Il carrello è vuoto</h2>
-                    <button class="btn">Torna ai piatti</button>
+                    <button class="btn"><a href="javascript:history.back()">Torna ai piatti</a></button>
                 </div>
                 <div v-else class="total">
                     <h2>Totale</h2>
@@ -81,7 +81,7 @@
                     <hr>
                     <p class="total-mobile"><b>Totale</b> € @{{ calculateTotal.toFixed(2) }}</p>
                     <div class="checkout-bottom" v-if="calculateTotal == 0">
-                        <button class="btn">Torna ai piatti</button>
+                        <button class="btn"><a href="javascript:history.back()">Torna ai piatti</a></button>
                     </div>
                 </div>
             </section>
