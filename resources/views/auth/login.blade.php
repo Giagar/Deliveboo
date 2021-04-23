939248f500b6d2abe2c8d3c953ac3324eac66e70
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-04-23 10:44:41 +0200

    modifiche colori e bottoni

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,14 +50,14 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="login form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-warning">
+                                <button type="submit" class="btn btn-dark btn-bg-black">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" style="color:orange;font-weight:bold;" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card" style="margin:50px 0;background-color:white;">
+            <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
@@ -166,9 +166,9 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="btn-register form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-warning">
+                                <button type="submit" class="btn btn-dark btn-bg-black">
                                     {{ __('Register') }}
                                 </button>
                             </div>
